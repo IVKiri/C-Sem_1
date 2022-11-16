5c02835a2f85db85a7ce751263800cd7f3e48716
Inna Kirianova <innr@ya.ru>
2022-11-16 23:29:01 +0300

    added code for task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+double x1 = -number;
+
+while(x1 < number)
+{
+    Console.Write(x1 + ", ");
+    x1 = x1+1;
+}
+Console.Write(number);
