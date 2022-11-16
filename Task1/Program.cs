2e96e1d03c4a50be93eed466031c54c32d8ac869
Inna Kirianova <innr@ya.ru>
2022-11-16 22:06:58 +0300

    added code for task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число 1: ");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA * numberA == numberB)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
