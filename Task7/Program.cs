e400375cf1cc654cc1d8e69bfb7a72e99ecb4f7f
Inna Kirianova <innr@ya.ru>
2022-11-16 23:45:13 +0300

    changed code for task 7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,5 +1,11 @@
 ﻿Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int result = number % 10;
-Console.WriteLine("Последняя цифра Вашего числа: " + result);
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Это не трёхзначное число!");
+}
+else {
+    int result = number % 10;
+    Console.WriteLine("Последняя цифра Вашего числа: " + result);
+}
